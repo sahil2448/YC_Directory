4c377d4e36bafa3a1b4a3abb1eae9e3cf7d00271
Sahil Kamble <syk2448@gmail.com>
2025-10-10 19:35:35 +0530

    building the startup details page

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -1,19 +1,40 @@
+import { formatDate } from '@/lib/utils';
 import { client } from '@/sanity/lib/client';
 import { STARTUP_BY_ID_QUERY } from '@/sanity/lib/queries';
+import Image from 'next/image';
+import Link from 'next/link';
 import { notFound } from 'next/navigation';
 
 export const experimental_ppr = true
-const page = async({params}:{params:Promise<{id:string}>}) => {
-    const id = (await params).id;
+const page = async ({ params }: { params: Promise<{ id: string }> }) => {
+  const id = (await params).id;
 
-    console.log(id)
-    const post = await client.fetch(STARTUP_BY_ID_QUERY,{id});
-    if(!post) return notFound();
+  console.log(id)
+  const post = await client.fetch(STARTUP_BY_ID_QUERY, { id });
+  if (!post) return notFound();
   return (
     <>
-       <h1 className='text-2xl'>{
-        post.title
-}</h1> 
+    <section className='w-full  min-h-[230px]  bg-pink-600  flex justify-center items-center flex-col py-10 px-6'>
+      <p className='bg-amber-400'>{formatDate(post?._createdAt)}</p>
+      <h1 className='uppercase bg-black px-6 py-3 font-work-sans font-extrabold text-white sm:text-[54px] sm:leading-[64px] text-[36px] leading-[46px] max-w-5xl text-center my-5'>
+        {post?.title}
+      </h1>
+      <p className='font-medium text-[20px] text-white  text-center break-words !max-w-5xl'>{post?.description}</p>
+    </section>
+      <section className='px-6 py-12 max-w-7xl mx-auto'>
+        <img src={post?.image} alt="thumbnail" className='w-full h-auto rounded-xl' />
+        <div className='space-y-5 mt-10 max-w-4xl mx-auto'>
+          <div className='flex justify-between gap-5'>
+            <Link href={`/user/${post.author?._id}`} className='flex gap-2 items-center mb-3'>
+              <Image src={post.author?.image} alt='avatar' width={64} height={64} className='rounded-full drop-shadow-lg'/>
+              <div>
+                <p className=''>{post.author.name}</p>
+                <p className=''>@{post.author.username}</p>
+              </div>
+            </Link>
+          </div>
+        </div>
+      </section>
     </>
   )
 }
